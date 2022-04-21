a0747d4eca7a8cf73b76e52be323c700a7b5896a
Charlie <75177875+TheTrueGravity@users.noreply.github.com>
2022-04-21 22:14:33 +0100

    main

--- src/json.js
+++ src/json.js
@@ -3,6 +3,12 @@ const {
     writeFileSync
 } = require('fs')
 
-module.exports.read = (path) => {
-    
+// Read a given JSON file
+module.exports.read = function (file) {
+    return JSON.parse(readFileSync(file))
+}
+
+// Write a given JSON file
+module.exports.write = function (file, data) {
+    writeFileSync(file, JSON.stringify(data, null, 4))
 }
